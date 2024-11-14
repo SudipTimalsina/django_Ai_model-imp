83274fffef466496cf99af61a778e58d55719ec8
SudipTimalsina <timalsinasudip11@gmail.com>
2024-11-14 13:46:46 +0545

    Added git ignote

--- home/models.py
+++ home/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 # Need to add a model to connect to the database
+class birdlist(models.Model):
+    name = models.CharField(max_length = 25)
+    scientificName = models.ChaField(max_length = 50)
+    image = models.ImageField()
+    birdUrl = models.URLField()
+    

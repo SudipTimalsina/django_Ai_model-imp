cf747429549266baad8f4f6775fa3b684f9ef7dc
SudipTimalsina <timalsinasudip11@gmail.com>
2024-11-14 12:16:40 +0545

    Displaying data in UI

--- home/views.py
+++ home/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    peoples = [
+        {'name':'Sudip', 'age': 20},
+        {'name':'Randeep', 'age': 22},
+        {'name':'Prakash', 'age': 32},
+        {'name':'Sandeep', 'age': 44},
+        {'name':'Vicky kaushal', 'age': 34}
+    ]
+
+    for i in peoples:
+        print(i)
+
+    return render(request ,"home/index.html", context ={'peoples':peoples})
+    
+
+def success_page(request):
+    return HttpResponse("Hey this is a success page")

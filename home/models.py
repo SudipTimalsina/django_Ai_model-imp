ea3f47164889d488b8cc1c47a0aaa8904fe5ada6
SudipTimalsina <timalsinasudip11@gmail.com>
2024-11-15 12:55:21 +0545

    model creation

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Need to add a model to connect to the database
 class birdlist(models.Model):
     name = models.CharField(max_length = 25)
-    scientificName = models.ChaField(max_length = 50)
+    scientificName = models.CharField(max_length = 50)
     image = models.ImageField()
     birdUrl = models.URLField()
     
